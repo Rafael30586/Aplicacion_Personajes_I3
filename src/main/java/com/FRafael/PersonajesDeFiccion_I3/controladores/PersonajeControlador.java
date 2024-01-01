c94b868e70613eb4c5482a605dbadd090882f335
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-01 15:43:10 -0300

    Formulario de agregado de personajes funcionando

--- src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/PersonajeControlador.java
+++ src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/PersonajeControlador.java
@@ -3,11 +3,13 @@ package com.FRafael.PersonajesDeFiccion_I3.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.FRafael.PersonajesDeFiccion_I3.entidades.Obra;
+import com.FRafael.PersonajesDeFiccion_I3.entidades.Personaje;
 import com.FRafael.PersonajesDeFiccion_I3.servicios.ObraServicio;
 import com.FRafael.PersonajesDeFiccion_I3.servicios.PersonajeServicio;
 import java.util.List;
@@ -25,11 +27,34 @@ public class PersonajeControlador {
 	public ModelAndView mostrarFormulario(Model model) {
 		
 		List<Obra> obras = obraServicio.devolverTodas();
-		
 		model.addAttribute("obras",obras);
 		
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("");
+		modelAndView.setViewName("formulario_personaje.html");
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/personaje-guardado")
+	public ModelAndView guardar(String nombre, String apodo, String idObra,
+			String fotoUrl,Model model) {
+		Long idObraLong = Long.parseLong(idObra);
+		
+		Obra obra = obraServicio.devolverPorId(idObraLong);
+		
+		List<Obra> obras = obraServicio.devolverTodas();
+		model.addAttribute("obras",obras);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("formulario_personaje.html");
+		
+		Personaje personaje = new Personaje();
+		personaje.setNombre(nombre);
+		personaje.setApodo(apodo);
+		personaje.setObra(obra);
+		personaje.setFotoUrl(fotoUrl);
+		
+		personajeServicio.guardar(personaje);
 		
 		return modelAndView;
 	}
