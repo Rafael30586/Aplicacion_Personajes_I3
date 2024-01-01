30a649d56d8db7c4e04b8ee257a31803b55b5375
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-01 10:13:50 -0300

    controlador y template para formulario de obra

--- src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/ObraControlador.java
+++ src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/ObraControlador.java
@@ -1,8 +1,10 @@
 package com.FRafael.PersonajesDeFiccion_I3.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.FRafael.PersonajesDeFiccion_I3.servicios.ObraServicio;
 
@@ -12,5 +14,13 @@ public class ObraControlador {
 	
 	@Autowired
 	private ObraServicio obraServicio;
+	
+	@GetMapping("/formulario")
+	public ModelAndView mostrarFormulario() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("formulario_obra.html");
+		
+		return modelAndView;
+	}
 
 }
