fd7bc298a01066e770a650e0e2bcc136a4778799
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-01 12:16:25 -0300

    Controlador de formulario para personajes

--- src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/PersonajeControlador.java
+++ src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/PersonajeControlador.java
@@ -1,10 +1,16 @@
 package com.FRafael.PersonajesDeFiccion_I3.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.FRafael.PersonajesDeFiccion_I3.entidades.Obra;
+import com.FRafael.PersonajesDeFiccion_I3.servicios.ObraServicio;
 import com.FRafael.PersonajesDeFiccion_I3.servicios.PersonajeServicio;
+import java.util.List;
 
 @RequestMapping("/personajes")
 @RestController
@@ -12,5 +18,20 @@ public class PersonajeControlador {
 	
 	@Autowired
 	private PersonajeServicio personajeServicio;
+	@Autowired
+	private ObraServicio obraServicio;
+	
+	@GetMapping("/formulario")
+	public ModelAndView mostrarFormulario(Model model) {
+		
+		List<Obra> obras = obraServicio.devolverTodas();
+		
+		model.addAttribute("obras",obras);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("");
+		
+		return modelAndView;
+	}
 
 }
