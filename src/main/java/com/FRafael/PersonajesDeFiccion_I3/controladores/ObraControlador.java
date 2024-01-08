e1b98f64918c48f0c7a43fcfec7104cd05236ec6
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-08 09:34:03 -0300

    Correccion en controlador para edicion de obra

--- src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/ObraControlador.java
+++ src/main/java/com/FRafael/PersonajesDeFiccion_I3/controladores/ObraControlador.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -154,11 +155,13 @@ public class ObraControlador {
 	}
 	
 	@PutMapping("/editada")
-	public void editarObra(@RequestParam Long id, @RequestParam String titulo, 
-			@RequestParam int anioLanzamiento) {
+	public void editarObra(@RequestParam Long id,@RequestBody Obra obraDatos) {
 		Obra obra = obraServicio.devolverPorId(id);
-		obra.setTitulo(titulo);
-		obra.setAnioLanzamiento(anioLanzamiento);
+		
+		obra.setTitulo(obraDatos.getTitulo());
+		obra.setAnioLanzamiento(obraDatos.getAnioLanzamiento());
+		obra.setClasificacion(obraDatos.getClasificacion());
+		
 		obraServicio.guardar(obra);
 	}
 
